d0c8cf78d8b9edd8f0a7a5097a34dfccc73b202e
DsevillanoNavarro <dsevillanonavarro123@gmail.com>
2024-11-27 12:56:00 +0100

    terminado

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=60)
+    cuerpo = models.TextField()
+    fecha = models.DateField(default='2005-6-18')
+    def __str__(self):
+        return str(self.id) + " " + str(self.titulo)
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def principal(request):
+    posts = Post.objects.all()
+    autores = Post.objects.all().values_list('autor',flat=True).distinct()
+    return render(request, 'blog/principal.html',{"posts":posts,
+                                                  "autores":autores})
+def menu(request):
+    posts = Post.objects.all().order_by('-fecha')
+    return render(request, 'blog/menu.html',{"posts":posts})
